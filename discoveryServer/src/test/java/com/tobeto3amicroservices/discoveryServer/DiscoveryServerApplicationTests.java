9f0f1d79d14ef94edb7f1150a0dc5571b3e7ec9c
pc <htccnbz06@gmail.com>
2024-03-12 23:58:44 +0300

    commit initial

---
+++ discoveryServer/src/test/java/com/tobeto3amicroservices/discoveryServer/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tobeto3amicroservices.discoveryServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
